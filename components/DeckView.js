db719e89dbb0a4c717145168a873e984a845e05a
Fabricio Brito <fabricio.brito@gmail.com>
2020-10-10 15:54:01 -0300

    feat: Add buttons as placeholders for actions

--- components/DeckView.js
+++ components/DeckView.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 export default class DeckView extends Component {
+  nextQuestion(direction) {
+
+  }
   render() {
     const { navigation } = this.props
     return(
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>10 Cards</Text>
         <View>
-          <Text>{'<'}</Text>
+          <TouchableOpacity onPress={() => this.nextQuestion(-1)}>
+            <Text>{'<'}</Text>
+          </TouchableOpacity>
           <Text>Question...</Text>
-          <Text>{'>'}</Text>
+          <TouchableOpacity onPress={() => this.nextQuestion(1)}>
+            <Text>{'>'}</Text>
+          </TouchableOpacity>
         </View>
         <TouchableOpacity onPress={() => navigation.navigate('AddCard')}>
           <Text>Add Card</Text>
--- components/QuizView.js
+++ components/QuizView.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 export default class QuizView extends Component {
+  state = {
+    showAnswer: false
+  }
+  revealAnswer() {
+    this.setState(() => ({showAnswer: true}))
+  }
   render() {
     const { navigation } = this.props
     return(
@@ -9,10 +15,23 @@ export default class QuizView extends Component {
         <Text>Card 1 out of 10</Text>
         <View>
           <Text>Question...</Text>
+          {this.state.showAnswer && (
+            <Text>Answer...</Text>
+          )}
         </View>
-        <TouchableOpacity>
-          <Text>View Answer</Text>
-        </TouchableOpacity>
+        {this.state.showAnswer
+          ? <View>
+              <TouchableOpacity>
+                <Text>Correct</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text>Incorrect</Text>
+              </TouchableOpacity>
+            </View>
+          : <TouchableOpacity onPress={() => this.revealAnswer()}>
+              <Text>View Answer</Text>
+            </TouchableOpacity>
+        }
         <TouchableOpacity onPress={() => navigation.navigate('ResultView')}>
           <Text>Finish Quiz</Text>
         </TouchableOpacity>
