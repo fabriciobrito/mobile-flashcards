722b4f1007d6e7fcf89d44adaed8e9d752f13f01
Fabricio Brito <fabricio.brito@gmail.com>
2020-10-12 15:53:49 -0300

    feat: Add icons to Tab Navigation Bar

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './HomeStack';
-import CreateDeck from './AddDeckStack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import HomeStack from './HomeStack';
+import AddDeckStack from './AddDeckStack';
 
 const Tab = createBottomTabNavigator();
 
 export default function Main() {
   return(
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Decks" component={Home} />
-        <Tab.Screen name="Add Deck" component={CreateDeck} />
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            switch(route.name){
+              case 'Decks':
+                iconName = 'cards';
+                break;
+              case 'Add Deck':
+                iconName = 'creation';
+            }
+
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          },
+        })}>
+        <Tab.Screen name="Decks" component={HomeStack} />
+        <Tab.Screen name="Add Deck" component={AddDeckStack} />
       </Tab.Navigator>
     </NavigationContainer>
   )
