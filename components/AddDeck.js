1d2fb3c6a1aca8ad3b79b1d26345ce140c7fd1ee
Fabricio Brito <fabricio.brito@gmail.com>
2020-10-12 15:54:02 -0300

    feat: Add style to AddDeck View

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { KeyboardAvoidingView, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { storeData } from '../utils/api';
-import { addDeck } from '../actions'
+import { addDeck } from '../actions';
+import NavButton from './NavButton';
 
 class AddDeck extends Component {
   state = {
@@ -27,24 +28,42 @@ class AddDeck extends Component {
   render() {
     const { deckName } = this.state;
     return(
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Enter the Deck Name</Text>
+      <KeyboardAvoidingView style={styles.container}>
+        <Text style={styles.header}>Enter the Deck Name</Text>
         <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          style={styles.input}
           onChangeText={(text) => this.onChangeText(text)}
           placeholder='Deck Name'
           value={deckName}
           maxLength={25}
           autoFocus={true}
         />
-        <TouchableOpacity
+        <NavButton
           onPress={this.addDeck}
-          disabled={deckName === ''}>
-          <Text>Submit</Text>
-        </TouchableOpacity>
-      </View>
+          disabled={deckName === ''}
+          text={'Submit'}/>
+      </KeyboardAvoidingView>
     )
   }
 }
 
 export default connect()(AddDeck);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    marginHorizontal: 20
+  },
+  header: {
+    fontSize: 18
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginVertical: 15
+  }
+})
