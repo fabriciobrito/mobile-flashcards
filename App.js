ba3b6ed3210c8b6a8b59f86f10c2761ea2a38e95
Fabricio Brito <fabricio.brito@gmail.com>
2020-10-13 02:02:54 -0300

    feat: Notifications installation attempt

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import * as React from 'react';
+import { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducers';
 import middleware from './middleware';
 import Main from './navigation/TabNavigator';
 
-export default function App() {
-  return (
-    <Provider store={createStore(reducer, middleware)}>
-      <Main />
-    </Provider>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer, middleware)}>
+        <Main />
+      </Provider>
+    );
+  }
 }
---
+++ utils/notification.js
@@ -0,0 +1,55 @@
+import * as Notifications from 'expo-notifications';
+import AsyncStorage from '@react-native-community/async-storage';
+
+//I've tried, but it turns out that Local Notification is legacy now...
+//https://docs.expo.io/versions/latest/sdk/legacy-notifications/#localnotification
+
+const NOTIFICATION_KEY = 'MobileFlashCards:notifications';
+
+export function clearLocalNotification () {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync)
+}
+
+function createNotification () {
+  return {
+    title: 'Time for Brain Exercise!',
+    body: "Review your mobile Flash Cards Today!",
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true,
+    }
+  }
+}
+
+export function setLocalNotification () {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync()
+
+              let tomorrow = new Date()
+              tomorrow.setDate(tomorrow.getDate() + 1)
+              tomorrow.setHours(20)
+              tomorrow.setMinutes(0)
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              )
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          })
+      }
+    })
+}
